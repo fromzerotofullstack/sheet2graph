3384218d51e03022680a1bb33489d16b2b3d8a15
mejutoco <j@mejuto.co>
2021-03-04 11:36:54 +0100

    graphing the first image from our data

--- mycommand.py
+++ mycommand.py
@@ -1,6 +1,13 @@
 import pandas as pd
+import plotly
+import plotly.express as px
 
 if __name__ == '__main__':
-    print("Hello, fromzerotofullstack")
-    df = pd.read_csv('sales_data.csv')
-    print(df)
+    df = pd.read_csv('sales_data.csv', header=None)
+    df.dropna(how='all', inplace=True)
+    df = df.iloc[2:]
+    df.columns = ['Salesman', 'Week1', 'Week2', 'Week3', 'Week4']
+    df['Week1'] = df['Week1'].astype(int)
+
+    fig = px.bar(df, x='Salesman', y='Week1')
+    plotly.io.write_image(fig, "output.png")
