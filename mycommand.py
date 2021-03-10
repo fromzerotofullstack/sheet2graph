82d7a7fb5ffea7c0fd3eaf3aa99c9c5be792aa2f
mejutoco <j@mejuto.co>
2021-03-10 15:38:02 +0100

    refactor into main

--- mycommand.py
+++ mycommand.py
@@ -4,13 +4,47 @@ import plotly.express as px
 import pathlib
 import argparse
 
+
+def main(input_file='', output_filename=None, output_format='', output_folder='', output_size='700x500', graph_type='bar'):
+
+    if output_filename:
+        p = pathlib.Path(output_filename)
+        output_folder = str(p.parent)
+        pathlib.Path(output_folder).mkdir(parents=True, exist_ok=True)
+        output_path = "{folder}/{f}".format(folder=output_folder, f=p.name)
+        # .png, .svg, etc.
+        output_format = p.suffix[1:]
+    else:
+        pathlib.Path(output_folder).mkdir(parents=True, exist_ok=True)
+        output_path = "{folder}/output.{ext}".format(folder=output_folder, ext=output_format)
+
+    tmp = output_size.split('x')
+    width, height = tmp[0], tmp[1]
+
+    df = pd.read_csv(input_file, header=None)
+    df.dropna(how='all', inplace=True)
+    df = df.iloc[2:]
+    df.columns = ['Salesman', 'Week1', 'Week2', 'Week3', 'Week4']
+    df['Week1'] = df['Week1'].astype(int)
+
+    if graph_type == 'bar':
+        fig = px.bar(df, x='Salesman', y='Week1')
+    elif graph_type == 'line':
+        fig = px.line(df, x='Salesman', y='Week1')
+    else:
+        raise Exception("graph type not supported")
+
+    plotly.io.write_image(fig, output_path, format=output_format, width=width, height=height)
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="""
     Graph spreadsheet data easily
     Takes a spreadsheet file (csv, xls) as input and outputs an image file (bitmap, vector) with gaphs of the data contained in the file
     """)
     parser.add_argument('input_file', nargs='+', help='input file (csv,xls)')
-    parser.add_argument('--graph-type', '-gt', nargs='?', dest='graph_type', default="bar", help='[bar|line]: default is bar')
+    parser.add_argument('--graph-type', '-gt', nargs='?', dest='graph_type', default="bar",
+                        help='[bar|line]: default is bar')
     parser.add_argument(
         '--output-folder', '-of', nargs='?', dest='output_folder', default="output",
         help="output_folder (ending without slash): default is 'output'. Can be serveral folders. ex. 'sales/graphs'"
@@ -30,36 +64,11 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     input_file = args.input_file[0]
-    output_filename = args.output_filename
-
-    if output_filename:
-        p = pathlib.Path(output_filename)
-        output_folder = str(p.parent)
-        pathlib.Path(output_folder).mkdir(parents=True, exist_ok=True)
-        output_path = "{folder}/{f}".format(folder=output_folder, f=p.name)
-        # .png, .svg, etc.
-        output_format = p.suffix[1:]
-    else:
-        output_folder = args.output_folder
-        pathlib.Path(output_folder).mkdir(parents=True, exist_ok=True)
-        output_format = args.output_format
-        output_path = "{folder}/output.{ext}".format(folder=output_folder, ext=output_format)
-
-    tmp = args.size.split('x')
-    width, height = tmp[0], tmp[1]
-
-
-    df = pd.read_csv(input_file, header=None)
-    df.dropna(how='all', inplace=True)
-    df = df.iloc[2:]
-    df.columns = ['Salesman', 'Week1', 'Week2', 'Week3', 'Week4']
-    df['Week1'] = df['Week1'].astype(int)
-
-    if args.graph_type == 'bar':
-        fig = px.bar(df, x='Salesman', y='Week1')
-    elif args.graph_type == 'line':
-        fig = px.line(df, x='Salesman', y='Week1')
-    else:
-        raise Exception("graph type not supported")
-
-    plotly.io.write_image(fig, output_path, format=output_format, width=width, height=height)
+    main(
+        input_file=input_file,
+        output_filename=args.output_filename,
+        output_format=args.output_format,
+        output_folder=args.output_folder,
+        output_size=args.size,
+        graph_type=args.graph_type,
+    )
