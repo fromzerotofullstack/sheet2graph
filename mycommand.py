8a8d53bd6ad515511c58e016bdc7a9efd86a4f18
mejutoco <j@mejuto.co>
2021-03-10 10:55:38 +0100

    save to folder

--- mycommand.py
+++ mycommand.py
@@ -1,8 +1,12 @@
 import pandas as pd
 import plotly
 import plotly.express as px
+import pathlib
 
 if __name__ == '__main__':
+    output_folder = 'output'
+    pathlib.Path(output_folder).mkdir(parents=True, exist_ok=True)
+
     df = pd.read_csv('sales_data.csv', header=None)
     df.dropna(how='all', inplace=True)
     df = df.iloc[2:]
@@ -10,4 +14,6 @@ if __name__ == '__main__':
     df['Week1'] = df['Week1'].astype(int)
 
     fig = px.bar(df, x='Salesman', y='Week1')
-    plotly.io.write_image(fig, "output.png")
+
+    output_path = "{folder}/output.png".format(folder=output_folder)
+    plotly.io.write_image(fig, output_path)
