021632b1b64a77a6a6a4b235d47b9e4acd8a4f67
mejutoco <j@mejuto.co>
2021-03-24 10:43:14 +0100

    helpers for testing

--- tests.py
+++ tests.py
@@ -1,13 +1,43 @@
+# only needed to run the tests
+import pathlib
+import shutil
 import unittest
+import os
+from PIL import Image
 
 
 class TestCommandLine(unittest.TestCase):
+    """
+    - file is crated in path
+    - google docs is opened
+    - files different formats work (img is generated)
+    - resolution of file
+    """
+    folder_output = '123_folder_output_tests'
+
+    @staticmethod
+    def file_exists(fp):
+        return os.path.isfile(fp)
+
+    @staticmethod
+    def get_image_resolution(fp):
+        im = Image.open(fp)
+        width, height = im.size
+        return width, height
+
+    @staticmethod
+    def run_cmd(params=''):
+        """
+        helper to run our command
+        """
+        cmd = 'env3.8/bin/python3.8 mycommand.py {params}'.format(params)
+        os.system(cmd)
 
     def setUp(self):
-        pass
+        pathlib.Path(self.folder_output).mkdir(parents=True, exist_ok=True)
 
     def tearDown(self):
-        pass
+        shutil.rmtree(self.folder_output)
 
     def test_first(self):
         self.assertEqual('test', 'test')
