ea257d416b758eddb3bcfb77a5033d77701bde9d
mejutoco <j@mejuto.co>
2021-03-30 12:03:16 +0200

    setup files
    - cfg for most of it
    - py for dependencies
        - deps are not pinned to exact versions

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='sheet2graph',
+    setup_requires=[
+        'kaleido',
+        'numpy',
+        'openpyxl',
+        'pandas',
+        'Pillow',
+        'plotly',
+        'requests',
+        'typeguard',
+    ],
+    install_requires=[
+        'kaleido',
+        'numpy',
+        'openpyxl',
+        'pandas',
+        'Pillow',
+        'plotly',
+        'requests',
+        'typeguard',
+    ]
+)
+
